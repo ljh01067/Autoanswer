0c042368ed925b3ed0af6ec9f549964ff0bf1299
ljh01067 <163291012+ljh01067@users.noreply.github.com>
2024-12-01 22:06:14 +0900

    Update and rename AnswerRepository.java to LocalRepository.java

---
+++ src/main/java/com/koreait/autoanswer1/repository/LocalRepository.java
@@ -0,0 +1,18 @@
+package com.koreait.resionweather1.repository;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface LocalRepository {
+    @Select("SELECT sigunguNm FROM location where areaNm='${areaNm}'")
+    public List<Map<String, Object>> findSigunguNm(String areaNm);
+
+    @Select("SELECT DISTINCT areaNm FROM location;")
+    public List<Map<String, Object>> findAreaList();
+
+    @Select("SELECT areaCd, signguCd FROM location WHERE areaNm = #{areaNm} AND sigunguNm = #{sigunguNm}")
+    List<Map<String, Integer>> findAreaCd(@Param("areaNm") String areaNm, @Param("sigunguNm") String sigunguNm);
+}
